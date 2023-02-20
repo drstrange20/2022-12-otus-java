4a2d6fec4273bc0128c243e1ad479d63d39bdfda
drstrange20 <fatiha.94@mail.ru>
2023-02-20 18:07:24 +0300

    hw05 v8

--- hw05-logging/src/main/java/my/homework/Demo.java
+++ hw05-logging/src/main/java/my/homework/Demo.java
@@ -5,8 +5,8 @@ import my.homework.api.service.TestLoggingInterface;
 import my.homework.service.Ioc;
 
 public class Demo {
-    public static void main(String[] args) {
-        TestLoggingInterface myClass = Ioc.createMyClass();
+    public static void main(String[] args) throws Exception {
+        TestLoggingInterface myClass = (TestLoggingInterface) Ioc.createMyClass("my.homework.service.TestLogging");
         myClass.calculation(1);
         myClass.calculation(1, 2);
         myClass.calculation(1, 2, "3");
--- hw05-logging/src/main/java/my/homework/service/Ioc.java
+++ hw05-logging/src/main/java/my/homework/service/Ioc.java
@@ -2,43 +2,54 @@ package my.homework.service;
 
 
 import my.homework.annotation.Log;
-import my.homework.api.service.TestLoggingInterface;
 import service.OutputService;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import java.lang.reflect.*;
 import java.util.Arrays;
 import java.util.List;
 
 
 public class Ioc {
-    private static final List<Method> METHODS = Arrays
-            .stream(TestLogging.class.getDeclaredMethods())
-            .filter(method -> method.isAnnotationPresent(Log.class)).toList();
+    private static List<Method> methods;
 
-    public static TestLoggingInterface createMyClass() {
-        InvocationHandler handler = new DemoInvocationHandler(new TestLogging());
+    public static void setMethods(List<Method> methods) {
+        Ioc.methods = methods;
+    }
 
-        return (TestLoggingInterface) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
-                new Class<?>[]{TestLoggingInterface.class}, handler);
+    public static List<Method> getMethods() {
+        return methods;
+    }
+
+    public static Object createMyClass(String className) throws Exception {
+        Class<?> clazz = Class.forName(className);
+        setMethods(Arrays
+                .stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(Log.class))
+                .toList());
+
+        Constructor<?> constructor = clazz.getConstructor();
+        Object object = constructor.newInstance();
+        InvocationHandler handler = new DemoInvocationHandler(object);
+
+        return Proxy.newProxyInstance(Ioc.class.getClassLoader(),
+                new Class<?>[]{clazz.getInterfaces()[0]}, handler);
     }
 
     static class DemoInvocationHandler implements InvocationHandler {
-        private final TestLoggingInterface myClass;
+        private final Object object;
         private final OutputService outputService = new ConsoleOutputService();
 
-        DemoInvocationHandler(TestLoggingInterface myClass) {
-            this.myClass = myClass;
+        DemoInvocationHandler(Object object) {
+            this.object = object;
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            for (Method method1 : METHODS) {
+            for (Method method1 : getMethods()) {
                 if (Arrays.equals(method1.getGenericParameterTypes(), method.getGenericParameterTypes()) && method1.getName().equals(method.getName())) {
                     outputService.outputMessage("executed method: " + method.getName() + ", params: " + Arrays.toString(args));
                 }
             }
-            return method.invoke(myClass, args);
+            return method.invoke(object, args);
         }
     }
 }
