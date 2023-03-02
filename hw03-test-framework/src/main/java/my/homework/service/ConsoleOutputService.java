3a7879148f67b88702085d46dc77f649bd910132
drstrange20 <fatiha.94@mail.ru>
2023-03-02 22:29:54 +0300

    hw05 v11

--- hw03-test-framework/src/main/java/my/homework/service/ConsoleOutputService.java
+++ hw03-test-framework/src/main/java/my/homework/service/ConsoleOutputService.java
@@ -1,7 +1,13 @@
 package my.homework.service;
 
-import service.OutputService;
+import service.ReportService;
 
 
-public class ConsoleOutputService implements OutputService {
+public class ConsoleOutputService implements ReportService {
+    @Override
+    public void printReport(String param1, String param2) {
+        outputMessage("TOTAL NUMBER OF TESTS: " + param1);
+        outputMessage("PASSED TESTS: " + (Integer.parseInt(param1) - Integer.parseInt(param2)));
+        outputMessage("FAILED TESTS: " + param2);
+    }
 }
